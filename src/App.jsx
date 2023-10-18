662066aa59995fdc9962834756967e252e8d1ea7
Deepak Murali <deepak.murali@mrisoftware.com>
2023-10-18 22:07:07 +0530

    Added the pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import React from "react";
-import { BrowserRouter, Router, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import Signin from "./pages/Signin";
+import Signup from "./pages/SignUp";
 export default function App() {
   return (
     <BrowserRouter>
-      <Route>
-        <Route path="/" element={Home} />
-      </Route>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/About" element={<About/>} />
+        <Route path="/Profile" element={<Profile/>} />
+        <Route path="/Signin" element={<Signin/>} />
+        <Route path="/Signup" element={<Signup/>} />
+
+      </Routes>
     </BrowserRouter>
   );
 }
