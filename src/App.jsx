8cf5386d3bf719ec2cb18f5f820362974b1c3038
Deepak Murali <deepak.murali@mrisoftware.com>
2023-10-18 23:49:59 +0530

    Added the header

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,19 @@ import About from "./pages/About";
 import Profile from "./pages/Profile";
 import Signin from "./pages/Signin";
 import Signup from "./pages/SignUp";
+import Header from "./components/Header";
 export default function App() {
   return (
     <BrowserRouter>
+    <Header/>
       <Routes>
         <Route path="/" element={<Home/>} />
         <Route path="/About" element={<About/>} />
         <Route path="/Profile" element={<Profile/>} />
         <Route path="/Signin" element={<Signin/>} />
         <Route path="/Signup" element={<Signup/>} />
-
       </Routes>
+      
     </BrowserRouter>
   );
 }
