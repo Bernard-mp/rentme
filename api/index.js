6e6ecffbbbf925988730a2e014232f45cefdc2d0
Deepak Murali <deepak.murali@mrisoftware.com>
2023-11-23 05:13:29 +0530

    database added - system2

--- api/index.js
+++ api/index.js
@@ -1,4 +1,16 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'; //for using .env file
+dotenv.config();
+//Getting from .env file and in mongo for password conataining @ will we encoded as %40 
+//https://www.eso.org/~ndelmott/url_encode.html refer this for url encoding
+mongoose.connect(process.env.MONGO).then(()=>
+    {
+        console.log("Connected to db");
+    }
+) .catch((err)=>   {
+    console.log(err, "error ");
+})
 const app = express();
 app.listen(3000,() =>{
 console.log("server is running")
