a69d9a07e36eb9479f1f9fca4834d3b6908ac181
Deepak Murali <deepakmp14@gmail.com>
2023-11-23 06:00:02 +0530

    api test is added - system2

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv'; //for using .env file
+import Userrouter from './route/user.route.js';
 dotenv.config();
 //Getting from .env file and in mongo for password conataining @ will we encoded as %40 
 //https://www.eso.org/~ndelmott/url_encode.html refer this for url encoding
@@ -15,3 +16,5 @@ const app = express();
 app.listen(3000,() =>{
 console.log("server is running")
 });
+
+app.use('/api/user',Userrouter);
